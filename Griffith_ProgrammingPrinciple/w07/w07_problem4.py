86c88a8ce98b24e5a0bdf18c44467bab78a5fdf5
hohoho <85296213+sosopark92@users.noreply.github.com>
2024-09-10 22:52:36 +1000

    Update w07_problem4.py

--- Griffith_ProgrammingPrinciple/w07/w07_problem4.py
+++ Griffith_ProgrammingPrinciple/w07/w07_problem4.py
@@ -7,24 +7,34 @@ import math
 dim1 = float(input("Enter room dmension 1 (m): "))
 dim2 = float(input("Enter room dmension 2 (m): "))
 
-#determine length and width of room
-if dim1 > dim2:
-   length = dim1
-   width = dim2
-else :
-   length = dim2
-   width = dim1
+while dim1 != 0 and dim1 != 0:
+      #determine length and width of room
+      if dim1 > dim2:
+         length = dim1
+         width = dim2
+      else :
+         length = dim2
+         width = dim1
+      print("Length of room = ", format(length, ".3f"), "m")
+      print("Width of room = ", format(width, ".3f"), "m")
 
-#calculate lengthways amount of carpet required
-carpet_width = 3.66
+      #amount of carpet sheets
+      carpet_width = 3.66
 
-#width: amount of carpet sheets
-sheets = math.ceil(width / cw) #ceil = round up
-sl = math.ceil(length) #sl = sheet length
-cl = sheets*sl #cl = carpet length
+      #calculate lengthways amount of carpet required
+      sheets = math.ceil(width / carpet_width) #ceil = round up
+      sl = length #sl = sheet length
+      cl = math.ceil(sl*sheets) #cl = carpet length
 
-#calculate widthways amount of carpet required
-sheets2 = math.ceil(length / cw) 
-sl2 = math.ceil(width) 
-cl2 = sheets*sl 
+      #calculate widthways amount of carpet required
+      sheets2= math.ceil(length / carpet_width) 
+      sl2 = width
+      cl2 = math.ceil(sl2*sheets2)
+
+      print("Total carpet length required in lengthways manner = ", cl, "m")
+      print("Total carpet length required in widthways manner = ", cl2, "m")
+
+
+
+
 
