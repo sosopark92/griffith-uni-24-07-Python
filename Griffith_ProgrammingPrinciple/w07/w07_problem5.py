ce5195c7925a7bcbbbe312c625e75bef7f12d9a2
hohoho <85296213+sosopark92@users.noreply.github.com>
2024-09-11 11:17:33 +1000

    Update w07_problem5.py

---
+++ Griffith_ProgrammingPrinciple/w07/w07_problem5.py
@@ -0,0 +1,45 @@
+
+# opening= int(input("Opening balance of account: "))
+# amount = int(input("Amount deposited or withdrew (negative value): "))
+
+# def balance(opening, amount):
+#     deposit = 0
+#     withdraw = 0
+
+#     while amount != 0:       
+#           if amount >0:
+#              deposit = deposit + amount
+#              withdraw = withdraw
+#           else:
+#             withdraw = withdraw - amount
+#             deposit = deposit
+          
+#           amount = int(input("Amount deposited or withdrew (negative value): "))
+
+#     print("Opening balance: ", opening)
+#     print("Deposits: ", deposit)
+#     print("Withdrawlas: ", withdraw)
+#     print("Closing balance: ", opening+deposit-withdraw)
+
+# balance(opening, amount)
+
+opening = int(input("Opening balance of account: "))
+amount = int(input("Amount deposited or withdrew (negative value): "))
+deposit = 0
+withdraw = 0
+
+def balance(opening, amount):
+    while amount != 0:
+        if amount > 0:
+           deposit += amount
+        else:
+           withdraw -= amount
+
+        amount = int(input("Amount deposited or withdrew (negative value): "))
+    
+    print("Opening balance:", opening)
+    print("Deposits:", deposit)
+    print("Withdrawals:", withdraw)
+    print("Closing balance:", opening + deposit - withdraw)
+
+balance(opening, amount)
