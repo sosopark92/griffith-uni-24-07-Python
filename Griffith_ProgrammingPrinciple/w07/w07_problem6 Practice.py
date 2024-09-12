59b6aa6593160198949f487b7b9caa2316b1d966
hohoho <85296213+sosopark92@users.noreply.github.com>
2024-09-12 17:17:48 +1000

    w07_problem06

--- Griffith_ProgrammingPrinciple/w07/w07_problem6 Practice.py
+++ Griffith_ProgrammingPrinciple/w07/w07_problem6 Practice.py
@@ -20,41 +20,29 @@ while pensReq > 0 and booksReq > 0:
          print("Number of pens in stock: ", pens)
          if pens <= 5:
             print("There are only 5 pens or less left.")
+      if pens <= 0:
+         print("Number of pens in stock: ", pens)                 
       
       if booksReq <= books:
          books -= booksReq
          print("Number of books in stock: ", books)
          if books <= 5:
             print("There are only 5 books or less left.")
+      if books <= 0:
+         print("Number of books in stock: ", books)  
 
       pensReq = int(input("Number of pens requested by customer: "))
-      booksReq = int(input("Number of books requested by customer: "))
-
-
       
-
-# while pensReq > 0 and booksReq > 0:
-#       if pensReq <= pens:
-#          pens -= pensReq
-#          print("Number of pens in stock: ", pens)
-#          if pens <= 5:
-#             print("There are only 5 pens or less left.")
-#       else:
-#          check = input(f"There are only {pens} pens in stock. Do you want to buy the {pens} pens? Y/N: ")
-#          if check == "Y":
-#             pens = 0
-#             print("New pens must be ordered")      
-
-#       if booksReq <= books:
-#          books -= booksReq
-#          print("Number of books in stock: ", books)
-#          if books <= 5:
-#             print("There are only 5 books or less left.")
-#       else: 
-#          check = input(f"There are only {books} books in stock. Do you want to buy the {books} books? Y/N: ")
-#          if check == "Y":
-#             books = 0
-#             print("New pens must be ordered")       
-            
-#       pensReq = int(input("Number of pens requested by customer: "))
-#       booksReq = int(input("Number of books requested by customer: "))
+      if pensReq <= pens:
+         booksReq = int(input("Number of books requested by customer: "))
+      else:
+         check = input(f"There are only {pens} pens in stock. Do you want to buy the {pens} pens? Y/N: ")
+         if check == "Y":
+            pens = 0
+            print("New pens must be ordered")            
+            booksReq = int(input("Number of books requested by customer: "))
+            if booksReq > books:
+               check = input(f"There are only {books} pens in stock. Do you want to buy the {books} pens? Y/N: ")
+               if check == "Y":
+                  books = 0
+                  print("New books must be ordered")            
